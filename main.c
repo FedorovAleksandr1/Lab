be4686e8bcbd20ab40ef2de28fa7a88814049c67
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-22 17:02:14 +0300

    Lab5Br

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
+
 #include <stdio.h>
 
 int main()
 {
-    // 10 - 100
-    // 6
-    int num, num2, m;
-    scanf("%d", &num);
-    printf((num >= 10 && num <= 100) ? "true\n": "false\n");
+    int arr[7] = {10, 20, 30, 40, 50, 60, 70};
+    printf("%d %d %d\n", arr[0], arr[1], arr[2]);
+    printf("%d %d %d\n", arr[3], arr[4], arr[5]);
+    printf("%d\n", arr[6]);
 
-    scanf(" %o", &num2);
-    printf("%c %d", num2 ? 'Y':'N',(m>>21) %2 );
+    int matrix1[2][2] = {{1, 1}, {1, 4}};
+    int matrix2[2][2] = {{1, 2}, {4, 1}};
+    int matrix12[2][2];
+    matrix12[0][0] = matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0];
+    matrix12[0][1] = matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1];
+    matrix12[1][0] = matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0];
+    matrix12[1][1] = matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1];
 
-
+    printf("%d %d \n%d %d", matrix12[0][0], matrix12[0][1], matrix12[1][0], matrix12[1][1]);
 }
+
