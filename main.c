4a396ad040adabd4d492fff256665fd7fdd0a16c
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-22 17:07:38 +0300

    Lab8Br

--- main.c
+++ main.c
@@ -1,51 +1,16 @@
-
 #include <stdio.h>
+#include <string.h>
+int main () {
+    int N;
+    int K;
+    scanf ("%d", N);
+    char str1 [N];
+    char str2 [K];
+    for (int i = 0; i < N; i++)
+        if (str1[i] == str2[i])
+        {
+            printf("Элемент совпадает, %d", i );
+        }
 
-int PI = 3.14159265;
-
-struct Circle {
-
-    int R;
-};
-
-int CircleS (struct Circle Circ) {
-    return 2 * (PI * Circ.R);
-}
-
-
-struct ADSL {
-    unsigned int DSL: 1;
-    unsigned int PPP: 1;
-    unsigned int Link: 1;
-};
-
-union ADSLU {
-    unsigned u;
-    struct ADSL ADSL;
-};
-
-int main()
-{
-    enum Days {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday,
-    };
-
-    printf("Day: %d \n", Monday);
-
-    struct Circle Circ = {7};
-    printf("Sqr: %d \n", CircleS(Circ));
-
-    union ADSLU ADSLState;
-    printf("Turn On: ");
-    scanf("%x", &ADSLState.u);
-    printf("Play: %d\n", ADSLState.ADSL.DSL);
-    printf("Play: %d\n", ADSLState.ADSL.PPP);
-    printf("Play: %d\n", ADSLState.ADSL.Link);
 }
 
