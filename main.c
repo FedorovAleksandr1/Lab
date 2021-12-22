001ded471f9467277a36a1b490d3ef7ba12760ec
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-22 17:03:59 +0300

    Lab6

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
-
 #include <stdio.h>
+#include <malloc.h>
 
 int main()
 {
-    int arr[7] = {10, 20, 30, 40, 50, 60, 70};
-    printf("%d %d %d\n", arr[0], arr[1], arr[2]);
-    printf("%d %d %d\n", arr[3], arr[4], arr[5]);
-    printf("%d\n", arr[6]);
+    char arr[4] = {'a', 'b', 'c', 'd'};
+    char *arr_c = arr;
+    for(int i=0; i<4 ; i++){
+        printf("%c ", *arr_c++);
+    }
+    printf("\n");
 
-    int matrix1[2][2] = {{1, 1}, {1, 4}};
-    int matrix2[2][2] = {{1, 2}, {4, 1}};
-    int matrix12[2][2];
-    matrix12[0][0] = matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0];
-    matrix12[0][1] = matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1];
-    matrix12[1][0] = matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0];
-    matrix12[1][1] = matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1];
-
-    printf("%d %d \n%d %d", matrix12[0][0], matrix12[0][1], matrix12[1][0], matrix12[1][1]);
+    char *arr_2 = (char *) malloc(4*sizeof(char));
+    arr_2[0] = 'a';
+    arr_2[1] = 'b';
+    arr_2[2] = 'c';
+    arr_2[3] = 'd';
+    for(int i=0; i<4; i++) {
+        printf("%с ", *arr_2++);
+    }
+    free(arr_2);
 }
 
