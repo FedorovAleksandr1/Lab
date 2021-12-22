495bd3ec9021801b987764f8a8f89330e4501d77
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-22 16:52:05 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/MainLab11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainLab11.dir/main.c.obj"
+  "MainLab11.exe"
+  "MainLab11.exe.manifest"
+  "MainLab11.pdb"
+  "libMainLab11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MainLab11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
